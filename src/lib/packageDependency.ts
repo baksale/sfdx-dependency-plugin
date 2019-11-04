42837163034ddca9bf03a6c5a2b602abfbeccdd2
baksale <java.rabota@gmail.com>
2019-11-04 08:38:52 +0300

    added caching to improve performance

--- src/lib/packageDependency.ts
+++ src/lib/packageDependency.ts
@@ -1,8 +1,10 @@
 import { Connection } from '@salesforce/core';
 import {EntityDependencyApi} from 'any-dependency-tree/dist/index'
-import { Package2Version, SubscriberPackageVersion } from './model';
+import { Package2Version, SubscriberPackageVersion, Dependencies } from './model';
 
 export class PackageDependencyApi implements EntityDependencyApi<Package2Version>{
+    private packageToDependencyIds: Map<string, string[]> = new Map();
+    private packages: Map<string, Package2Version> = new Map();
     private packageVersionQuery = 'select Id,SubscriberPackageVersionId,Package2.Name,Package2.NamespacePrefix'
         +',Tag,Branch,MajorVersion,MinorVersion,PatchVersion,BuildNumber,IsReleased,IsPasswordProtected'
         +' from Package2Version ';
@@ -16,32 +18,45 @@ export class PackageDependencyApi implements EntityDependencyApi<Package2Version
     public async getPackagesByIds(packageIds: string[]): Promise<Package2Version[]>{
         const result: Package2Version[] = [];
         if(packageIds.length == 0) return result;
-        const packageWhereClause = this.packageVersionWhereClause.replace('%s', packageIds.join(','));
+        const missingPackageIds: string[] = [];
+        packageIds.forEach(id =>{
+            if(!this.packages.has(id)) missingPackageIds.push(id);
+            else result.push(this.packages.get(id));
+        });
+        const packageWhereClause = this.packageVersionWhereClause.replace('%s', missingPackageIds.join(','));
         const packagesQuery = this.packageVersionQuery + packageWhereClause + this.packageVersionOrderByClause;
         await this.connection.tooling.query<Package2Version>(packagesQuery)
             .then(packageQueryResult => {
                 packageQueryResult.records.forEach(packageVersion => {
                     result.push(packageVersion);
+                    this.packages.set('\'' + packageVersion.SubscriberPackageVersionId + '\'', packageVersion);
                 })
-            });        
+            });
         return result;
     }
     
     public async getEntityDependencies(entity: Package2Version): Promise<Package2Version[]> {
-        const result: Package2Version[] = [];
+        let result: Package2Version[] = [];
         if(entity.IsPasswordProtected) return result;
+        else if(this.packageToDependencyIds.has(entity.SubscriberPackageVersionId)){
+            return await this.getPackagesByIds(this.packageToDependencyIds.get(entity.SubscriberPackageVersionId));
+        }
         const packageDependencyIds: string[] = [];
         const dependenciesWhereClause = this.packageDependenciesWhereClause.replace('%s', entity.SubscriberPackageVersionId);
         const dependenciesQuery = this.packageDependenciesQuery + dependenciesWhereClause;
         await this.connection.tooling.query<SubscriberPackageVersion>(dependenciesQuery)
             .then(subscriberPackageVersion => {
                 subscriberPackageVersion.records.forEach(packageVersion => {
-                    packageVersion.Dependencies&&packageVersion.Dependencies.ids.forEach(id =>{
-                        packageDependencyIds.push('\'' + id.subscriberPackageVersionId + '\'');
-                    })
+                    if(packageVersion.Dependencies){
+                        packageVersion.Dependencies&&packageVersion.Dependencies.ids.forEach(id =>{
+                            packageDependencyIds.push('\'' + id.subscriberPackageVersionId + '\'');
+                        })
+                        this.packageToDependencyIds.set(entity.SubscriberPackageVersionId, packageDependencyIds);
+                    }
                 });
             });
-        return await this.getPackagesByIds(packageDependencyIds);
+        result = await this.getPackagesByIds(packageDependencyIds);
+        return result;
     }
     
 }
