023eb479f70e75a2ea29ecb279f8642788cb2830
baksale <java.rabota@gmail.com>
2019-11-14 08:26:49 +0300

    enabled name display by default for tree

--- src/commands/dependency/tree.ts
+++ src/commands/dependency/tree.ts
@@ -43,7 +43,7 @@ export default class Tree extends SfdxCommand {
   protected static flagsConfig = {
     // flag with a value (-p, --package=VALUE)
     package: flags.string({char: 'p', description: messages.getMessage('packageFlagDescription')}),
-    name: flags.boolean({char: 'n', description: messages.getMessage('nameFlagDescription'), default: false}),
+    name: flags.boolean({char: 'n', description: messages.getMessage('nameFlagDescription'), default: true}),
     version: flags.boolean({description: messages.getMessage('packageVersionDescription'), default: false}),
     id: flags.boolean({description: messages.getMessage('idFlagDescription'), default: false})
   };
